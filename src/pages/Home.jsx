c4763c414fb5dc5d0e8b3c078b6cdf65627a3204
hemanth <ehemanth709#gmail.com>
2026-06-06 22:02:32 +0530

    Fix FiguresPreview filename

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import HeroSection from "../components/home/HeroSection";
 import FeaturedEmpires from "../components/home/FeaturedEmpires";
 import TimelinePreview from "../components/home/TimelinePreview";
-import Figurespreview from "../components/home/FiguresPreview";
+import Figurespreview from "../components/home/Figurespreview";
 import DailyChallenge from "../components/home/DailyChallenge";
 import AchievementSection from "../components/home/AchievementSection";
 import Navbar from "../components/Navbar";
